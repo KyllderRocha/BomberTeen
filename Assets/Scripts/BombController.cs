49d83efe93e8c91abc9d680ddfd7fa94da30d2d5
KyllderRocha <kyllder.rocha@gmail.com>
2023-05-03 19:29:28 -0300

    MVP

---
+++ Assets/Scripts/BombController.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
+
+public class BombController : MonoBehaviour
+{
+    /*
+     * Melhorias
+     * 
+     * Colisão Com player
+     * Colisão com bomba fixa
+     * Explosão afetar outras bombas
+     * Explosão apagar power up
+     * 
+    */
+
+    [Header("Bomb")]
+    public KeyCode inputKey = KeyCode.Space;
+    public GameObject bombPrefab;
+    public float bombFuseTime = 3.0f;
+    public int bombAmount = 1;
+    private int bombsRemaining = 0;
+
+    [Header("Explosion")]
+    public Explosion explosionPrefab;
+    public LayerMask explosionLayerMask;
+    public float explosianDuration = 1f;
+    public int explosionRadius = 1;
+
+    [Header("Destructible")]
+    public Tilemap destructibleTiles;
+    public Destructible destructiblePrefab;
+
+
+    private void OnEnable()
+    {
+        bombsRemaining = bombAmount;
+    }
+
+    private void Update()
+    {
+        if(bombsRemaining > 0 && Input.GetKeyDown(inputKey))
+        {
+            StartCoroutine(PlaceBomb());
+        }
+        
+    }
+
+    private IEnumerator PlaceBomb()
+    {
+        Vector2 position = transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+
+        GameObject bomb = Instantiate(bombPrefab, position, Quaternion.identity);
+        bombsRemaining--;
+
+        yield return new WaitForSeconds(bombFuseTime);
+
+        position = bomb.transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(explosion.start);
+        explosion.DestroyAfter(explosianDuration);
+
+        Explode(position, Vector2.up, explosionRadius);
+        Explode(position, Vector2.down, explosionRadius);
+        Explode(position, Vector2.left, explosionRadius);
+        Explode(position, Vector2.right, explosionRadius);
+
+        Destroy(bomb);
+        bombsRemaining++;
+
+
+    }
+
+    private void Explode(Vector2 position, Vector2 direction, int legth )
+    {
+        if (legth <= 0)
+            return;
+
+        position += direction;
+
+        if(Physics2D.OverlapBox(position, Vector2.one/ 2f, 0f, explosionLayerMask))
+        {
+            ClearDestructible(position);
+            return;
+        }
+
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(legth > 1 ? explosion.middle : explosion.end);
+        explosion.SetDirection(direction);
+        explosion.DestroyAfter(explosianDuration);
+
+        Explode(position, direction, legth -1);
+    }
+
+    private void ClearDestructible(Vector2 position)
+    {
+        Vector3Int cell = destructibleTiles.WorldToCell(position);
+        TileBase tile = destructibleTiles.GetTile(cell);
+
+        if (tile != null)
+        {
+            Instantiate(destructiblePrefab, position, Quaternion.identity);
+            destructibleTiles.SetTile(cell, null);
+        }
+    }
+
+    public void AddBomb()
+    {
+        bombAmount++;
+        bombsRemaining++;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Bomb"))
+        {
+            other.isTrigger = false;
+        }
+    }
+}
